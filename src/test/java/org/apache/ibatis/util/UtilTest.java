a2f6f376590447d668df557b6e4b722a2a4ff204
Dxh7788 <anan1215>
2018-07-18 15:16:07 +0800

    properties属性文件测试工具2

--- src/test/java/org/apache/ibatis/util/UtilTest.java
+++ src/test/java/org/apache/ibatis/util/UtilTest.java
@@ -14,9 +14,34 @@ import java.util.Properties;
 public class UtilTest {
     @Test
     public void propsTest() throws IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        InputStream in = classLoader.getResourceAsStream("org/apache/ibatis/databases/blog/blog-mysql.properties");
+        //系统classLoader
+        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+        //当前线程的classLoader
+        ClassLoader threadLoader = Thread.currentThread().getContextClassLoader();
+        //当前类的classLoader
+        ClassLoader currentClassLoader = getClass().getClassLoader();
+        if (systemClassLoader == threadLoader){
+            System.out.println("equals 1");
+        }
+        if (currentClassLoader == threadLoader){
+            System.out.println("equals 2");
+        }
+        if (systemClassLoader == currentClassLoader){
+            System.out.println("equals 3");
+        }
+        InputStream in = currentClassLoader.getResourceAsStream("org/apache/ibatis/databases/blog/blog-mysql.properties");
+        InputStream in2 = threadLoader.getResourceAsStream("org/apache/ibatis/databases/blog/blog-mysql.properties");
+        InputStream in3 = systemClassLoader.getResourceAsStream("org/apache/ibatis/databases/blog/blog-mysql.properties");
         Properties props = new Properties();
+        if (in2 != null){
+            props.load(in2);
+            System.out.println(props.getProperty("password"));
+        }
+        if (in3 != null){
+            props.load(in3);
+            System.out.println(props.getProperty("driver"));
+        }
+
         props.load(in);
         String value = props.getProperty("username");
         System.out.println(value);
