100a48af0dcc354162f7560459ec315240052e8a
Dxh7788 <anan1215>
2018-07-30 16:43:47 +0800

    ObjectFactory源码分析

--- src/test/java/org/apache/ibatis/reflection/ObjectFactoryTest.java
+++ src/test/java/org/apache/ibatis/reflection/ObjectFactoryTest.java
@@ -18,12 +18,6 @@ public class ObjectFactoryTest {
         MetaObject object = MetaObject.forObject(new Person(),new DefaultObjectFactory(),new DefaultObjectWrapperFactory(),factory);
         object.setValue("user.user.name","123");
         User iu = (User)object.getValue("user");
-        System.out.println(iu.getName());
-//        MetaClass metaClass = MetaClass.forClass(User.class, factory);
-//        Invoker invoker = metaClass.getSetInvoker("name");
-//        Object o = new String("5566");
-//        Object[] os = {o};
-//        invoker.invoke(u, os);
-//        System.out.println(u.getName());
+        System.out.println(iu.getUser().getName());
     }
 }
