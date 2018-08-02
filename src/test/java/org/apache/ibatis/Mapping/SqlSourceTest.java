429658938a955460798d4973d657bc2b34891e81
Dxh7788 <anan1215>
2018-08-02 18:08:38 +0800

    sql

---
+++ src/test/java/org/apache/ibatis/Mapping/SqlSourceTest.java
@@ -0,0 +1,17 @@
+package org.apache.ibatis.Mapping;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author xh.d
+ * @since 2018/8/2 16:54
+ */
+public class SqlSourceTest {
+
+    @Test
+    public void test(){
+        //首先使用SqlSourceBuilder进行解析
+
+    }
+}
