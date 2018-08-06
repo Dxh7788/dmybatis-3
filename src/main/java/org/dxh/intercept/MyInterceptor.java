1cef8db80c055486e562206f271da75b484bc134
Dxh7788 <anan1215>
2018-08-06 18:12:42 +0800

    拦截器

---
+++ src/main/java/org/dxh/intercept/MyInterceptor.java
@@ -0,0 +1,39 @@
+package org.dxh.intercept;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.plugin.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+/**
+ * Copyright (C) 2017-2018 https://www.htouhui.com - A project by mybatis
+ *
+ * @author xh.d
+ * @since 2018/8/6 17:51
+ */
+@Intercepts({@Signature(type = StatementHandler.class,method = "getBoundSql",args = {})})
+public class MyInterceptor implements Interceptor {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+        BoundSql boundSql = statementHandler.getBoundSql();
+        String sql = boundSql.getSql();
+        System.out.println("mybatis intercept sql:{"+sql+"}");
+        logger.info("mybatis intercept sql:{}", sql);
+        return invocation.proceed();
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Plugin.wrap(target,this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
